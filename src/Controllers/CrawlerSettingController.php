d0cdb89de464ee47a744304035c8de47be68c531
Nguyễn Hoàng Duy <duynguyenvt97@gmail.com>
2024-02-23 10:35:47 +0700

    Update CrawlerSettingController.php

--- src/Controllers/CrawlerSettingController.php
+++ src/Controllers/CrawlerSettingController.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace KKPhim\Crawler\KKPhimCrawler\Controllers;
+namespace Ophim\Crawler\KKPhimCrawler\Controllers;
 
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\Settings\app\Models\Setting;
 use Illuminate\Support\Facades\Route;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 use Illuminate\Http\Request;
-use KKPhim\Crawler\KKPhimCrawler\Option;
+use Ophim\Crawler\KKPhimCrawler\Option;
 use Prologue\Alerts\Facades\Alert;
 
 class CrawlerSettingController extends CrudController
